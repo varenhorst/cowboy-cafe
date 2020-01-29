7a8add3d62e4e5d3d2b881a262b07471b1802378
Alex Varenhorst <apvarenh@users.campus>
2020-01-29 14:37:27 -0600

    Added Ribs Class

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the Ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+    }
+}
